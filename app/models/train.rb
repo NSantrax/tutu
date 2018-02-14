7c7efc9cd42277bb806f822e800c135dfec89470
NSantrax <ns2008p@mail.ru>
2018-02-14 22:57:26 +0300

    first commit

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,7 +1,7 @@
 class Train < ActiveRecord::Base
 	validates :number, presence: true
 
-	belongs_to :route
-	belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
+	belongs_to :route, optional: true
+	belongs_to :current_station, optional: true, class_name: 'RailwayStation', foreign_key: :current_station_id
         has_many :tickets
 end
