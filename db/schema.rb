9a673552296fe4954210fcaf04d99ff896dd51b0
NSantrax <ns2008p@mail.ru>
2018-05-19 17:11:15 +0300

    add index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424053137) do
+ActiveRecord::Schema.define(version: 20180519140105) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180424053137) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+   
   end
 
   create_table "tickets", force: :cascade do |t|
@@ -92,6 +93,7 @@ ActiveRecord::Schema.define(version: 20180424053137) do
     t.datetime "updated_at", null: false
     t.integer "train_id"
     t.integer "number_on_train", default: 0
+    t.index ["id", "type"], name: "index_wagons_on_id_and_type"
     t.index ["train_id"], name: "index_wagons_on_train_id"
   end
 
