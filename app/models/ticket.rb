a82b4904327a38596dd2a220884a226e50e37984
NSantrax <ns2008p@mail.ru>
2018-05-29 21:10:50 +0300

    letter del and edit ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,7 +6,9 @@ class Ticket < ActiveRecord::Base
    belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id
    belongs_to :train
    
-   after_create :send_notifiction
+   after_create :send_notification
+   after_destroy :send_destroy
+   after_update :send_change
    
    def route_name
      "#{base_station.title} - #{end_station.title}"
@@ -14,7 +16,16 @@ class Ticket < ActiveRecord::Base
    
    private
    
-   def send_notifiction
+   def send_notification
      TicketsMailer.bay_ticket(self.user, self).deliver_now
    end
+   
+   def send_destroy
+     TicketsMailer.del_ticket(self.user, self).deliver_now
+   end
+   
+   def send_change
+     TicketsMailer.edit_ticket(self.user, self).deliver_now
+   end
+   
 end
