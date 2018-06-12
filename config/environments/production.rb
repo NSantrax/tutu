9b3da528abf4dc7db05be2d5b7a17183b624d60d
NSantrax <ns2008p@mail.ru>
2018-06-12 13:25:49 +0300

    mail settings3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailler.default_url_options = { host: "secure-cliffs-46845.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "secure-cliffs-46845.herokuapp.com" }
    # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
